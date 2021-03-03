754c9cceadd9998df87f339f2f123276790a46ff
Pietro Longinetti <pietro.longi@gmail.com>
2021-03-03 12:45:28 +0100

    Starting to work on home page

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  shops = [
+    {
+      MBLink: '',
+      name: 'The best shop',
+      valutation: 4,
+      address: 'Via Roma 2, 50125',
+      hours: [
+        {sunday: 'closed'},
+        {monday: '8:00-12:00/14:00-17:00'},
+        {tuesday: '8:00-12:00/14:00-17:00'},
+        {wednesday: '8:00-12:00/14:00-17:00'},
+        {thursday: '8:00-12:00/14:00-17:00'},
+        {friday: '8:00-12:00/14:00-17:00'},
+        {saturday: '8:00-12:00'},
+      ]
+    },
+    {
+      MBLink: '',
+      name: 'The best of the best shops',
+      valutation: 5,
+      address: 'Via Fantechi 25, 50133',
+      hours: [
+        {sunday: 'closed'},
+        {monday: '8:00-13:00'},
+        {tuesday: '8:00-12:00/14:00-17:00'},
+        {wednesday: '8:00-12:00/14:00-17:00'},
+        {thursday: '8:00-12:00/14:00-17:00'},
+        {friday: '8:00-12:00/14:00-17:00'},
+        {saturday: '8:00-12:00'},
+      ]
+
+    }
+  ]
 
   constructor() {}
+
+  openShop(shopCard: any){
+    shopCard = shopCard.parentNode;
+    console.log(shopCard.childNodes[0].childNodes[0].textContent);
+  }
 
 }
