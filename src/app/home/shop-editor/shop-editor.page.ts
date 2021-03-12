894d985c69e463e1587286bf7948f3d7b2b481eb
pietro-hp <pietro.longinetti@hotmail.it>
2021-03-12 14:44:06 +0100

    Shop-editor patch: delete alert + scroll event fixed

--- src/app/home/shop-editor/shop-editor.page.ts
+++ src/app/home/shop-editor/shop-editor.page.ts
@@ -17,13 +17,14 @@ export class ShopEditorPage implements OnInit {
 
   constructor(private activatedRoute: ActivatedRoute, private router: Router, private exService: ShopDataExchangeService,
     private alertController: AlertController, private animationCtrl: AnimationController) {
-      this.id = this.activatedRoute.snapshot.paramMap.get('id');
-      this.exService.getShop(this.id).subscribe(shop => {this.shop = shop});
-      this.modifications = JSON.parse(JSON.stringify(this.shop)); //Deep copy
+    this.id = this.activatedRoute.snapshot.paramMap.get('id');
+    this.exService.getShop(this.id).subscribe(shop => { this.shop = shop });
+    this.modifications = JSON.parse(JSON.stringify(this.shop)); //Deep copy
   }
   ngOnInit() {
   }
 
+  // Alerts 
   async confirmAlert() {
     if (this.modified) {
       const alert = await this.alertController.create({
@@ -69,11 +70,33 @@ export class ShopEditorPage implements OnInit {
       })
       await alert.present();
     }
-    else{
+    else {
       this.router.navigate(['/tabs/home/shop/' + this.id]);
     }
   }
+  async deleteAlert() {
+    document.getElementsByTagName('ion-content')[0].scrollToTop(100);
+    const alert = await this.alertController.create({
+      backdropDismiss: false,
+      header: 'Are you sure?',
+      message: 'Do you really want delete this shop? All your data will be lost.',
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+        {
+          text: 'Yes',
+          handler: () => {
+            // TODO: delete data
+            this.router.navigate(['/tabs/home']);
+          }
+        }
+      ]
+    })
+    await alert.present();
+  }
 
+  // Methods
   async deletePhoto(imgId: number) {
     this.modified = true;
     await this.animationCtrl.create()
@@ -84,17 +107,17 @@ export class ShopEditorPage implements OnInit {
     this.modifications.imgs.splice(imgId, 1);
   }
   loadNewTurn(i: number) {
-    this.modified = true; 
-    this.modifications.hours[i].push({from: '', to: ''});
+    this.modified = true;
+    this.modifications.hours[i].push({ from: '', to: '' });
   }
-  async removeTurn(i: number, t:number){
+  async removeTurn(i: number, t: number) {
     this.modified = true;
     await this.animationCtrl.create()
-      .addElement(document.getElementById('day' + i).getElementsByTagName('ion-row')[this.modifications.hours[i].length -1])
+      .addElement(document.getElementById('day' + i).getElementsByTagName('ion-row')[this.modifications.hours[i].length - 1])
       .duration(350)
       .fromTo('opacity', '1', '0')
       .fromTo('height', '35.3px', '0')
       .play()
-    this.modifications.hours[i].splice(t,1);
+    this.modifications.hours[i].splice(t, 1);
   }
 }
